d97e89cf00b7c43d5a9b66d037ccddc7a4e1e403
Clockworks <40514153+Clockworkx@users.noreply.github.com>
2020-04-05 07:53:05 +0200

    ~include normal data

--- index.js
+++ index.js
@@ -183,8 +183,10 @@ module.exports = function(config = {}) {
             brDetails.br = {};
             brDetails.br_dmz = {};
             brDetails.br_all = {};
+            brDetails.b
             this.MWmp(gamertag, platform).then((data) => {
-                if(typeof data.lifetime !== "undefined") {
+                if (typeof data !== "undefined") { data = "data"; brDetails.data = data; }
+                else if(typeof data.lifetime !== "undefined") {
                     if(typeof data.lifetime.mode.br !== "undefined") { data.lifetime.mode.br.properties.title = "br"; brDetails.br = data.lifetime.mode.br.properties; }
                     if(typeof data.lifetime.mode.br_dmz !== "undefined") { data.lifetime.mode.br_dmz.properties.title = "br_dmz"; brDetails.br_dmz = data.lifetime.mode.br_dmz.properties; }
                     if(typeof data.lifetime.mode.br_all !== "undefined") { data.lifetime.mode.br_all.properties.title = "br_all"; brDetails.br_all = data.lifetime.mode.br_all.properties; }
